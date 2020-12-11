d69cc8bbf620e8bb14d0a63370522fe69a785eb8
skalia0594 <skalia@sfu.ca>
2020-12-11 18:35:59 -0500

    Product component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 //feature 1
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">eCommerce</a>
-      </header>
-      <main>
-        Product List
-      </main>
-      <footer>
-        All right is reserved.
-      </footer>
-    </div>
-  );
+import React from "react";
+import Products from "./components/Products";
+import data from "./data.json";
+class App extends React.Component {
+  constructor(props) {
+    super();
+    this.state={
+      products: data.products,
+      sort : "",
+      size : ""
+    }
+  }
+  render(){
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">eCommerce</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main-content">
+              <Products products={this.state.products} />
+            </div>
+            <div className="side-bar">Cart</div>
+          </div>
+        </main>
+        <footer>
+          All right is reserved.
+        </footer>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
