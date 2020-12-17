574adf9c66776845f104e19cba199a7d49986bde
skalia0594 <skalia@sfu.ca>
2020-12-17 18:38:31 -0500

    map filter component to redux

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from "react";
 import Cart from "./components/Cart";
 import Filter from "./components/Filter";
 import Products from "./components/Products";
-import data from "./data.json";
 import store from "./store";
 import {Provider} from "react-redux";
 
@@ -11,10 +10,7 @@ class App extends React.Component {
   constructor(props) {
     super();
     this.state={
-      products : data.products,
       cartItems : JSON.parse(localStorage.getItem("cartItems")) || [],
-      sort : "",
-      size : ""
     }
   }
   createOrder = (order) => {
@@ -42,34 +38,34 @@ class App extends React.Component {
     });
     localStorage.setItem("cartItems", JSON.stringify(cartItems));
   }
-  filterSize = e => {
-    if(e.target.value){ 
-      this.setState({
-        size: e.target.value,
-        products : data.products.filter(product => 
-                      product["availableSizes"].includes(e.target.value)
-                  ),
-        sort:""
-      })
-    }else {  // all case
-      this.setState({
-        size: e.target.value,
-        products: data.products
-      });
-    }
-  }
-  sortPrice = e => {
-    // console.log(e.target.value);
-    this.setState((prevState)=>({
-      sort: e.target.value,
-      products : prevState.products.slice().sort((a,b) =>(
-        e.target.value === "Highest" ?(a["price"]<b["price"]? 1:-1) : 
-        e.target.value === "Lowest" ?(a["price"]>b["price"]? 1:-1) :
-        (a["_id"]>b["_id"]? 1:-1)
-      )),
-    }));
+  // filterSize = e => {
+  //   if(e.target.value){ 
+  //     this.setState({
+  //       size: e.target.value,
+  //       products : data.products.filter(product => 
+  //                     product["availableSizes"].includes(e.target.value)
+  //                 ),
+  //       sort:""
+  //     })
+  //   }else {  // all case
+  //     this.setState({
+  //       size: e.target.value,
+  //       products: data.products
+  //     });
+  //   }
+  // }
+  // sortPrice = e => {
+  //   // console.log(e.target.value);
+  //   this.setState((prevState)=>({
+  //     sort: e.target.value,
+  //     products : prevState.products.slice().sort((a,b) =>(
+  //       e.target.value === "Highest" ?(a["price"]<b["price"]? 1:-1) : 
+  //       e.target.value === "Lowest" ?(a["price"]>b["price"]? 1:-1) :
+  //       (a["_id"]>b["_id"]? 1:-1)
+  //     )),
+  //   }));
       
-  }
+  // }
   render(){
     return (
       <Provider store= {store}>
@@ -80,13 +76,8 @@ class App extends React.Component {
           <main>
             <div className="content">
               <div className="main-content">
-                <Filter count={this.state.products.length} 
-                        sort={this.state.sort} 
-                        size={this.state.size}
-                        filterSize={this.filterSize}
-                        sortPrice={this.sortPrice}
-                />
-                <Products products={this.state.products} addToCart={this.addToCart}/>
+                <Filter />
+                <Products addToCart={this.addToCart}/>
               </div>
               <div className="side-bar">
                 <Cart cartItems= {this.state.cartItems} removeItem= {this.removeItem} createOrder= {this.createOrder}/>
